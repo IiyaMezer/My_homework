4213c157f90c0932c1a2884dc541899b41cb93b5
IiyaMezer <102888192+IiyaMezer@users.noreply.github.com>
2022-04-28 11:29:55 +0700

    ДЗ 1
    Выполнил дз 1.

---
+++ Lesson1/ConsoleApp1/HW1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class HW1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя:");
+            string Name = Console.ReadLine();
+            DateTime currDate = DateTime.Now;
+
+            Console.WriteLine($"Здравствуйте, {Name}, сегодня {currDate} ");
+
+            Console.WriteLine("Press ENTER to finish programm");
+            Console.ReadLine();
+        }
+    }
+}
